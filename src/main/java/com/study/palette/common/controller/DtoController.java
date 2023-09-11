b1f01d7671ad6d4232e684dca2c64452f3054574
ssohee <prinso@naver.com>
2023-09-11 17:32:34 +0900

    feat(common-dto):add error api - #11

--- src/main/java/com/study/palette/common/controller/DtoController.java
+++ src/main/java/com/study/palette/common/controller/DtoController.java
@@ -2,10 +2,8 @@ package com.study.palette.common.controller;
 
 import com.study.palette.common.dto.PaginationDto;
 import com.study.palette.common.dto.PagingDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.study.palette.common.exception.CustomException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,5 +14,15 @@ public class DtoController {
         PagingDto pagingDto = new PagingDto(2,1,10,0,false,true);
         String[] array = {"안녕","ㅇㅇ"};
         return PaginationDto.of(pagingDto,array);
+    }
+
+    @GetMapping(value = "/test")
+    public String test(@RequestParam(required = false) String param) throws Exception {
+
+        if(param.equals("2")) {
+            //ArithmeticException 발생
+            int div = 1/0;
+        }
+        return "success";
     }
 }
--- src/main/java/com/study/palette/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study/palette/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.study.palette.common.exception;
 
 import com.study.palette.common.dto.ErrorDto;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Timestamp;
+import java.util.stream.Stream;
 
 import static com.study.palette.common.constants.ErrorCode.INTERNAL_SERVER_ERROR;
 
@@ -18,10 +22,16 @@ import static com.study.palette.common.constants.ErrorCode.INTERNAL_SERVER_ERROR
 public class GlobalExceptionHandler {
 
 
+    private String getUri(){
+        String uri = ServletUriComponentsBuilder.fromCurrentRequest().toUriString();
+        int i = uri.indexOf('/',7);
+        return uri.substring(i);
+    }
+
     @ExceptionHandler({CustomException.class})
     protected ResponseEntity handleCustomException(CustomException ex){
         return new ResponseEntity(new ErrorDto(ex.getErrorCode().getStatus(), new Timestamp(System.currentTimeMillis()).toString(),
-                ServletUriComponentsBuilder.fromCurrentRequestUri().toString(),ex.getErrorCode().getMessage()),
+                getUri(),ex.getErrorCode().getMessage()),
                 HttpStatus.valueOf(ex.getErrorCode().getStatus()));
 
     }
@@ -29,7 +39,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({Exception.class})
     protected ResponseEntity handleServerException(Exception ex){
         return new ResponseEntity(new ErrorDto(INTERNAL_SERVER_ERROR.getStatus(),new Timestamp(System.currentTimeMillis()).toString(),
-                ServletUriComponentsBuilder.fromCurrentRequestUri().toString(),INTERNAL_SERVER_ERROR.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
+                getUri(),INTERNAL_SERVER_ERROR.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
