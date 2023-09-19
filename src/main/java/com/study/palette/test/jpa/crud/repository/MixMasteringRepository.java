39877f6c4ae8a0513ed4b63436143bd76780dac0
djhek <djheksa@gmail.com>
2023-09-19 09:20:29 +0900

    feat : test 폴더 삭제

--- src/main/java/com/study/palette/test/jpa/crud/repository/MixMasteringRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.palette.test.jpa.crud.repository;
-
-import com.study.palette.test.jpa.crud.model.MixMasteringInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MixMasteringRepository extends JpaRepository<MixMasteringInfo, String>, MixMasteringRepositoryCustom {
-
-}
