92396cb26c666f2d24a385f77fef8dd9063610df
kyoongdev <9898junjun2@gmail.com>
2023-10-08 21:30:28 +0900

    feat(review): review files

---
+++ src/main/java/com/study/palette/TokenGenerator.java
@@ -0,0 +1,50 @@
+package com.study.palette;
+
+import com.study.palette.authentication.domain.JwtTokenProvider;
+import com.study.palette.module.user.entity.Role;
+import com.study.palette.module.user.entity.User;
+import com.study.palette.module.user.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Log4j2
+public class TokenGenerator implements CommandLineRunner {
+
+  private final UserRepository userRepository;
+  private final JwtTokenProvider jwtTokenProvider;
+  private final PasswordEncoder passwordEncoder;
+
+
+  @Autowired
+  public TokenGenerator(UserRepository userRepository, JwtTokenProvider jwtTokenProvider, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.jwtTokenProvider = jwtTokenProvider;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    // 여기에 초기 데이터 생성 로직을 작성
+    User data = User.builder()
+            .email("testEmail")
+            .password(passwordEncoder.encode("testPassword"))
+            .role(Role.MEMBER)
+            .name("testName")
+            .phone("testPhone")
+            .isAlarmAccept(true)
+            .loginFailCount(0)
+            .isLocked(false)
+            .build();
+    userRepository.save(data);
+
+    Authentication authentication = jwtTokenProvider.getAuthentication(data.getId().toString());
+    log.info(jwtTokenProvider.createToken(authentication).getAccessToken());
+
+  }
+}
--- src/main/java/com/study/palette/module/user/UserService.java
+++ src/main/java/com/study/palette/module/user/UserService.java
@@ -6,138 +6,128 @@ import com.study.palette.module.user.dto.*;
 import com.study.palette.module.user.entity.Role;
 import com.study.palette.module.user.entity.User;
 import com.study.palette.module.user.repository.UserRepository;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.beans.PropertyDescriptor;
-import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-    //    public final MusicianService musicianService; TODO 서비스 구현시 추가
-    @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+  //    public final MusicianService musicianService; TODO 서비스 구현시 추가
+  @Autowired
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  //유저생성
+  @Transactional
+  public UserProfileDto createUser(UserCreateRequestDto userCreateRequestDto) {
+    Optional<User> user = getUserByEmail(userCreateRequestDto.getEmail());
+
+    if (user.isPresent()) {
+      throw new IllegalArgumentException("이미 존재하는 회원입니다.");
     }
 
-    //유저생성
-    @Transactional
-    public UserProfileDto createUser(UserCreateRequestDto userCreateRequestDto) {
-        Optional<User> user = getUserByEmail(userCreateRequestDto.getEmail());
+    User newUser = userRepository.save(
+            User.builder()
+                    .role(Role.MEMBER) // User 에서는 권한을 MEMBER 로 고정
+                    .email(userCreateRequestDto.getEmail())
+                    .password(passwordEncoder.encode(userCreateRequestDto.getPassword()))
+                    .name(userCreateRequestDto.getName())
+                    .phone(userCreateRequestDto.getPhone())
+                    .isAlarmAccept(userCreateRequestDto.isAlarmAccept())
+                    .loginFailCount(0)
+                    .isLocked(false)
+                    .build());
 
-        if (user.isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
-        }
+    return UserProfileDto.builder()
+            .id(userRepository.save(newUser).getId().toString())
+            .role(newUser.getRole())
+            .email(newUser.getEmail())
+            .password(newUser.getPassword())
+            .name(newUser.getName())
+            .phone(newUser.getPhone())
+            .isAlarmAccept(newUser.isAlarmAccept())
+            .loginFailCount(newUser.getLoginFailCount())
+            .isLocked(newUser.isLocked())
+            .build();
+  }
 
-        User newUser = userRepository.save(
-                User.builder()
-                        .role(Role.MEMBER) // User 에서는 권한을 MEMBER 로 고정
-                        .email(userCreateRequestDto.getEmail())
-                        .password(passwordEncoder.encode(userCreateRequestDto.getPassword()))
-                        .name(userCreateRequestDto.getName())
-                        .phone(userCreateRequestDto.getPhone())
-                        .isAlarmAccept(userCreateRequestDto.isAlarmAccept())
-                        .loginFailCount(0)
-                        .isLocked(false)
-                        .createdAt(LocalDateTime.now())
-                        .build());
+  //회원조회 ID
+  public Optional<User> getUserById(String id) {
+    return userRepository.findById(UUID.fromString(id));
+  }
 
-        return UserProfileDto.builder()
-                .id(userRepository.save(newUser).getId().toString())
-                .role(newUser.getRole())
-                .email(newUser.getEmail())
-                .password(newUser.getPassword())
-                .name(newUser.getName())
-                .phone(newUser.getPhone())
-                .isAlarmAccept(newUser.isAlarmAccept())
-                .loginFailCount(newUser.getLoginFailCount())
-                .isLocked(newUser.isLocked())
-                .createdAt(newUser.getCreatedAt().toString())
-                .build();
-    }
+  //회원조회 ID with DTO
+  public MyInfoResponseDto getUserByIdWithDto(String id) {
+    User user = userRepository.findById(UUID.fromString(id))
+            .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
 
-    //회원조회 ID
-    public Optional<User> getUserById(String id) {
-        return userRepository.findById(UUID.fromString(id));
-    }
+    MyInfoResponseDto myInfoResponseDto = MyInfoResponseDto.builder()
+            .user(user)
+            .build();
 
-    //회원조회 ID with DTO
-    public MyInfoResponseDto getUserByIdWithDto(String id) {
-        User user = userRepository.findById(UUID.fromString(id))
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
-
-        MyInfoResponseDto myInfoResponseDto = MyInfoResponseDto.builder()
-                .user(user)
-                .build();
-
-        if (user.getRole() == Role.MUSICIAN) {
-            // TODO 음악인 정보 조회
+    if (user.getRole() == Role.MUSICIAN) {
+      // TODO 음악인 정보 조회
 //            myInfoResponseDto.setMusician(musicianService.getMusicianByIdWithDto(id));
-        }
-
-        return myInfoResponseDto;
     }
 
-    //아이디찾기 Name and Phone with DTO
-    public UserFindEmailDto getUserByNameAndPhoneWithDto(String name, String phone) {
-        User user = userRepository.findByNameAndPhone(name, phone)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
+    return myInfoResponseDto;
+  }
 
-        return UserFindEmailDto.builder()
-                .name(user.getName())
-                .phone(user.getPhone())
-                .build();
-    }
+  //아이디찾기 Name and Phone with DTO
+  public UserEmailDto getUserByNameAndPhoneWithDto(String name, String phone) {
+    User user = userRepository.findByNameAndPhone(name, phone)
+            .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
+    
+    return UserEmailDto.builder()
+            .email(user.getEmail())
+            .build();
+  }
 
-    //회원조회 email
-    public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
+  //회원조회 email
+  public Optional<User> getUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
 
-    //회원정보 수정
-    @Transactional
-    public void updateUser(String id, UserUpdateDto userUpdateDto) {
-        User user = getUserById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
+  //회원정보 수정
+  @Transactional
+  public void updateUser(String id, UserUpdateDto userUpdateDto) {
+    User user = getUserById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
 
-        PaletteUtils.myCopyProperties(userUpdateDto, user);
+    PaletteUtils.myCopyProperties(userUpdateDto, user);
 
-        userRepository.save(user);
-    }
+    userRepository.save(user);
+  }
 
-    //삭제시간 생성 - soft
-    @Transactional
-    public void generateDeletedAt(String id) {
-        User user = getUserById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
+  //삭제시간 생성 - soft
+  @Transactional
+  public void generateDeletedAt(String id) {
+    User user = getUserById(id)
+            .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다."));
 
-        user.generateDeletedAt();
+    user.generateDeletedAt();
 
-        userRepository.save(user);
-    }
+    userRepository.save(user);
+  }
 
-    //회원 영구 삭제 - hard
-    @Transactional
-    public void deleteUser(String id) {
-        userRepository.deleteById(UUID.fromString(id));
-    }
+  //회원 영구 삭제 - hard
+  @Transactional
+  public void deleteUser(String id) {
+    userRepository.deleteById(UUID.fromString(id));
+  }
 
 
-    //TODO 회원목록조회 ?
+  //TODO 회원목록조회 ?
 
 
 }
