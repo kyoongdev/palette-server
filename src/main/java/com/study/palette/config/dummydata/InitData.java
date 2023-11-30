a990b40d5c75f35c9068f605d81032fb962dbb34
djhek <djheksa@gmail.com>
2023-11-30 09:27:36 +0900

    feat : dummydata 생성 분기처리

--- src/main/java/com/study/palette/config/dummydata/InitData.java
+++ src/main/java/com/study/palette/config/dummydata/InitData.java
@@ -36,15 +36,16 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Profile("local")
+//@Profile("local")
+@Log4j2
 public class InitData implements ApplicationRunner {
 
   private final ArtistRepository artistRepository;
@@ -59,13 +60,15 @@ public class InitData implements ApplicationRunner {
   /* 더미데이터 생성 시 new 연산자를 사용하거나 builder 패턴을 사용해서 데이터를 만들어준 뒤 reposiotry에 save (최초 한번만 실행 후 주석 처리) 추후 문제 처리 하겠습니다.*/
   @Override
   public void run(ApplicationArguments args) throws Exception {
-    Users initCommUser = usersRepository.save(
-        Users.builder()
-            .role(Role.MUSICIAN)
-            .email("test@test")
-            .password(passwordEncoder.encode("test1234"))
-            .name("홍길동")
-            .build());
+    //dummydata 생성 분기처리
+    boolean isExists = usersRepository.findByEmail("test@test").isPresent();
+
+    if (isExists) {
+      log.info("이미 더미데이터가 존재합니다.");
+      return;
+    }
+
+    Users initCommUser = usersRepository.save(Users.builder().role(Role.MUSICIAN).email("test@test").password(passwordEncoder.encode("test1234")).name("홍길동").build());
 
     /* artist */
     List<CreateArtistLicenseDto> artistLicenseInfo = new ArrayList<>();
@@ -73,67 +76,29 @@ public class InitData implements ApplicationRunner {
     List<CreateArtistContactDto> artistContacts = new ArrayList<>();
     List<ArtistInfo> artistInfoList = new ArrayList<>();
 
-    for(int i = 0 ; i < 10; i ++) {
+    for (int i = 0; i < 10; i++) {
 
       for (int j = 0; j < 3; j++) {
-        artistLicenseInfo.add(new CreateArtistLicenseDto(
-            10,
-            1000,
-            "servedFile" + i,
-            3,
-            1,
-            1,
-            true,
-            true,
-            true
-        ));
+        artistLicenseInfo.add(new CreateArtistLicenseDto(10, 1000, "servedFile" + i, 3, 1, 1, true, true, true));
       }
 
       for (int j = 0; j < 3; j++) {
-        artistContacts.add(new CreateArtistContactDto(
-            Contact.findContact(j + 1),
-            "010-1234-1234"
-        ));
+        artistContacts.add(new CreateArtistContactDto(Contact.findContact(j + 1), "010-1234-1234"));
       }
 
       for (int j = 0; j < 3; j++) {
-        artistFiles.add(new CreateArtistFileDto(
-            "www.test.com",
-            "testfilenale",
-            "uploadfilname",
-            1000,
-            "jpg",
-            true
-        ));
+        artistFiles.add(new CreateArtistFileDto("www.test.com", "testfilenale", "uploadfilname", 1000, "jpg", true));
       }
 
-      CreateArtistDto createArtistDto = new CreateArtistDto(
-          "serviceName",
-          "serviceInfo",
-          "editInfo",
-          2,
-          true,
-          LocalDate.now(),
-          artistFiles,
-          artistLicenseInfo,
-          artistContacts);
+      CreateArtistDto createArtistDto = new CreateArtistDto("serviceName", "serviceInfo", "editInfo", 2, true, LocalDate.now(), artistFiles, artistLicenseInfo, artistContacts);
 
       ArtistInfo artistInfo = createArtistDto.toEntity(initCommUser);
 
-      List<ArtistLicenseInfo> artistLicenseInfos = createArtistDto.getArtistLicenseInfo()
-          .stream()
-          .map(license -> ArtistLicenseInfo.from(license, artistInfo))
-          .toList();
+      List<ArtistLicenseInfo> artistLicenseInfos = createArtistDto.getArtistLicenseInfo().stream().map(license -> ArtistLicenseInfo.from(license, artistInfo)).toList();
 
-      List<ArtistContact> artistContactList = createArtistDto.getArtistContactDto()
-          .stream()
-          .map(contact -> ArtistContact.from(contact, artistInfo))
-          .toList();
+      List<ArtistContact> artistContactList = createArtistDto.getArtistContactDto().stream().map(contact -> ArtistContact.from(contact, artistInfo)).toList();
 
-      List<ArtistFile> artistFileList = createArtistDto.getArtistFileDto()
-          .stream()
-          .map(file -> ArtistFile.from(file, artistInfo))
-          .toList();
+      List<ArtistFile> artistFileList = createArtistDto.getArtistFileDto().stream().map(file -> ArtistFile.from(file, artistInfo)).toList();
 
       artistInfo.setArtistLicenseInfo(artistLicenseInfos);
       artistInfo.setArtistContact(artistContactList);
@@ -162,72 +127,26 @@ public class InitData implements ApplicationRunner {
       //albumart
 
       albumArtFileCreateRequestDtos.add(//앨범아트 파일 생성
-          new AlbumArtFileCreateRequestDto(
-              "www.test.com",
-              "testfilenale",
-              "uploadfilname",
-              1000,
-              "jpg",
-              true
-          ));
+          new AlbumArtFileCreateRequestDto("www.test.com", "testfilenale", "uploadfilname", 1000, "jpg", true));
       for (int j = 0; j < 3; j++) {
         albumArtLicenseCreateRequestDtos.add(//앨범아트 라이센스 생성
-            new AlbumArtLicenseInfoCreateRequestDto(
-                10,
-                1000,
-                "servedFile" + i,
-                3,
-                null,
-                3,
-                true,
-                true,
-                true,
-                true
-            ));
+            new AlbumArtLicenseInfoCreateRequestDto(10, 1000, "servedFile" + i, 3, null, 3, true, true, true, true));
 
         albumArtFileCreateRequestDtos.add(//앨범아트 파일 생성
-            new AlbumArtFileCreateRequestDto(
-                "www.test.com",
-                "testfilenale",
-                "uploadfilname",
-                1000,
-                "jpg",
-                false
-            ));
+            new AlbumArtFileCreateRequestDto("www.test.com", "testfilenale", "uploadfilname", 1000, "jpg", false));
       }
 
       for (int j = 0; j < 5; j++) {
         AlbumArtContactCreateDtos.add(//앨범아트 연락수단 생성
-            new AlbumArtContactCreateDto(
-                Contact.findContact(j + 1),
-                "content" + j
-            ));
+            new AlbumArtContactCreateDto(Contact.findContact(j + 1), "content" + j));
       }
 
-      AlbumArtCreateRequestDto albumArtCreateRequestDto = new AlbumArtCreateRequestDto(
-          "serviceName" + i,
-          "serviceExplain",
-          2,
-          "editInfo",
-          albumArtLicenseCreateRequestDtos,
-          AlbumArtContactCreateDtos,
-          albumArtFileCreateRequestDtos,
-          true
-      );
+      AlbumArtCreateRequestDto albumArtCreateRequestDto = new AlbumArtCreateRequestDto("serviceName" + i, "serviceExplain", 2, "editInfo", albumArtLicenseCreateRequestDtos, AlbumArtContactCreateDtos, albumArtFileCreateRequestDtos, true);
 
       AlbumArtInfo albumArtInfo = albumArtCreateRequestDto.toEntity(initCommUser);
-      List<AlbumArtLicenseInfo> licenses = albumArtCreateRequestDto.getAlbumArtLicenseInfo()
-          .stream()
-          .map(license -> AlbumArtLicenseInfo.from(license, albumArtInfo))
-          .toList();
-      List<AlbumArtContact> contacts = albumArtCreateRequestDto.getAlbumArtContact()
-          .stream()
-          .map(contact -> AlbumArtContact.from(contact, albumArtInfo))
-          .toList();
-      List<AlbumArtFile> files = albumArtCreateRequestDto.getAlbumArtFiles()
-          .stream()
-          .map(file -> AlbumArtFile.from(file, albumArtInfo))
-          .toList();
+      List<AlbumArtLicenseInfo> licenses = albumArtCreateRequestDto.getAlbumArtLicenseInfo().stream().map(license -> AlbumArtLicenseInfo.from(license, albumArtInfo)).toList();
+      List<AlbumArtContact> contacts = albumArtCreateRequestDto.getAlbumArtContact().stream().map(contact -> AlbumArtContact.from(contact, albumArtInfo)).toList();
+      List<AlbumArtFile> files = albumArtCreateRequestDto.getAlbumArtFiles().stream().map(file -> AlbumArtFile.from(file, albumArtInfo)).toList();
       albumArtInfo.setAlbumArtLicenseInfo(licenses);
       albumArtInfo.setAlbumArtContact(contacts);
       albumArtInfo.setAlbumArtFiles(files);
@@ -240,61 +159,14 @@ public class InitData implements ApplicationRunner {
       List<MixMasteringContact> mixMasteringContacts = new ArrayList<>();
       List<MixMasteringLicenseInfo> mixMasteringLicenseInfos = new ArrayList<>();
 
-      MixMasteringInfo mixMasteringInfo = MixMasteringInfo.builder()
-          .serviceName("믹스 마스터링" + Integer.toString(i))
-          .beforeJobMusic("")
-          .afterJobMusic("")
-          .serviceExplain("")
-          .editInfo("")
-          .serviceStatus(true)
-          .genre((i % 5) + 1)
-          .mixMasteringLicenseInfos(mixMasteringLicenseInfos)
-          .mixMasteringContacts(mixMasteringContacts)
-          .users(initCommUser)
-          .build();
+      MixMasteringInfo mixMasteringInfo = MixMasteringInfo.builder().serviceName("믹스 마스터링" + Integer.toString(i)).beforeJobMusic("").afterJobMusic("").serviceExplain("").editInfo("").serviceStatus(true).genre((i % 5) + 1).mixMasteringLicenseInfos(mixMasteringLicenseInfos)
+          .mixMasteringContacts(mixMasteringContacts).users(initCommUser).build();
 
-      mixMasteringContacts.add(MixMasteringContact.builder()
-          .type(1)
-          .content("010-1234-1234")
-          .mixMasteringInfo(mixMasteringInfo)
-          .build());
+      mixMasteringContacts.add(MixMasteringContact.builder().type(1).content("010-1234-1234").mixMasteringInfo(mixMasteringInfo).build());
 
-      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder()
-          .licenseType(1)
-          .price(10000)
-          .servedType("mp3")
-          .period(0)
-          .draftCount(1)
-          .isAssign(true)
-          .isUseCommercial(true)
-          .isServeOriginFile(false)
-          .isOtherUseApproved(false)
-          .mixMasteringInfo(mixMasteringInfo)
-          .build());
-      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder()
-          .licenseType(2)
-          .price(15000)
-          .servedType("mp3")
-          .period(0)
-          .draftCount(1)
-          .isAssign(true)
-          .isUseCommercial(true)
-          .isServeOriginFile(true)
-          .isOtherUseApproved(false)
-          .mixMasteringInfo(mixMasteringInfo)
-          .build());
-      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder()
-          .licenseType(3)
-          .price(20000)
-          .servedType("mp3")
-          .period(0)
-          .draftCount(1)
-          .isAssign(true)
-          .isUseCommercial(true)
-          .isServeOriginFile(true)
-          .isOtherUseApproved(true)
-          .mixMasteringInfo(mixMasteringInfo)
-          .build());
+      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder().licenseType(1).price(10000).servedType("mp3").period(0).draftCount(1).isAssign(true).isUseCommercial(true).isServeOriginFile(false).isOtherUseApproved(false).mixMasteringInfo(mixMasteringInfo).build());
+      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder().licenseType(2).price(15000).servedType("mp3").period(0).draftCount(1).isAssign(true).isUseCommercial(true).isServeOriginFile(true).isOtherUseApproved(false).mixMasteringInfo(mixMasteringInfo).build());
+      mixMasteringLicenseInfos.add(MixMasteringLicenseInfo.builder().licenseType(3).price(20000).servedType("mp3").period(0).draftCount(1).isAssign(true).isUseCommercial(true).isServeOriginFile(true).isOtherUseApproved(true).mixMasteringInfo(mixMasteringInfo).build());
 
       mixMasteringInfos.add(mixMasteringInfo);
     }
@@ -308,11 +180,7 @@ public class InitData implements ApplicationRunner {
       int randomValue2 = random.nextInt(20 - 1) + 1;
       UUID serviceId = albumArtRepository.findByServiceName("serviceName" + randomValue2).getId();
 
-      AlbumArtRequest albumArtRequest = AlbumArtRequest.builder()
-          .albumArtInfo(albumArtRepository.findById(serviceId).get())
-          .users(initCommUser)
-          .createAt(LocalDateTime.now())
-          .build();
+      AlbumArtRequest albumArtRequest = AlbumArtRequest.builder().albumArtInfo(albumArtRepository.findById(serviceId).get()).users(initCommUser).createAt(LocalDateTime.now()).build();
 
       albumArtRequestRepository.save(albumArtRequest);
     }
