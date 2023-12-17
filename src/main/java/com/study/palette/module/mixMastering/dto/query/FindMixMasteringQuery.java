cbf784dd77cc300a440857a2c740f5004e70bf7a
djhek <djheksa@gmail.com>
2023-12-17 16:04:05 +0900

    feat : 검색 로직 수정

--- src/main/java/com/study/palette/module/mixMastering/dto/query/FindMixMasteringQuery.java
+++ src/main/java/com/study/palette/module/mixMastering/dto/query/FindMixMasteringQuery.java
@@ -1,29 +1,17 @@
 package com.study.palette.module.mixMastering.dto.query;
 
 
-import com.querydsl.core.types.OrderSpecifier;
-import com.study.palette.common.dto.PageDto;
-import com.study.palette.common.enums.CustomSort;
-import com.study.palette.common.enums.mixMastering.MixMasteringGenre;
-import com.study.palette.module.mixMastering.entity.QMixMasteringInfo;
-import com.study.palette.module.mixMastering.entity.QMixMasteringRequest;
+import com.study.palette.module.mixMastering.service.MixMasteringConditions;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
+import javax.validation.constraints.Min;
 
-@Data
-public class FindMixMasteringQuery extends PageDto {
+public class FindMixMasteringQuery extends MixMasteringConditions {
 
-  @Schema(description = "전문장르", defaultValue = "", example = "1 : BALAD, 2 : HIPHOP, 3 : TROT, 4 : DANCE, 5 : RNB, 6 : ROCK, 7 : INDI, 8 : FORK, 9 : ETC")
-  public MixMasteringGenre genre;
+  @Schema(description = "전문장르", defaultValue = "0")
+  @Min(value = 0, message = "전문장르는 0 ~ 4 까지만 가능합니다.")
+  public int genre;
 
-  @Schema(description = "정렬", example = "1 : 신규등록, 2 : 인기순")
-  private CustomSort customSort;
-
-  public OrderSpecifier<?>[] getSort() {
-    if (this.customSort == CustomSort.POPULAR) {
-      return new OrderSpecifier[]{QMixMasteringRequest.mixMasteringRequest.id.count().desc()};
-    } else { // 신규등록
-      return new OrderSpecifier[]{QMixMasteringInfo.mixMasteringInfo.createdAt.desc()};
-    }
-  }
+  @Schema(description = "정렬", defaultValue = "0")
+  @Min(value = 0, message = "정렬은 0 ~ 3 까지만 가능합니다.")
+  private int customSort;
 }
--- src/main/java/com/study/palette/module/mixMastering/repository/MixMasteringCustomRepository.java
+++ src/main/java/com/study/palette/module/mixMastering/repository/MixMasteringCustomRepository.java
@@ -2,10 +2,11 @@ package com.study.palette.module.mixMastering.repository;
 
 import com.study.palette.module.mixMastering.dto.MixMasteringsDto;
 import com.study.palette.module.mixMastering.dto.query.FindMixMasteringQuery;
+import com.study.palette.module.mixMastering.service.MixMasteringConditions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MixMasteringCustomRepository {
 
-  Page<MixMasteringsDto> findAll(FindMixMasteringQuery query, Pageable pageable);
+  Page<MixMasteringsDto> findAll(MixMasteringConditions query, Pageable pageable);
 }
