b2bf89b8276a1b24e73aa91ed724f484db608721
sooyoung <sim980416@gmail.com>
2024-02-06 10:24:06 +0900

    fix : users issue

--- src/main/java/com/study/palette/module/users/dto/UserUpdateDto.java
+++ src/main/java/com/study/palette/module/users/dto/UserUpdateDto.java
@@ -1,5 +1,6 @@
 package com.study.palette.module.users.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,20 +10,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserUpdateDto {
 
+  @Schema(description = "닉네임", example = "nickname")
+  private String nickname;
 
-  private String email;
-  private String name;
-  @Size(max = 11, message = "전화번호는 11자리를 넘을 수 없습니다.")
-  private String phone;
-  private boolean isAlarmAccept;
+  @Schema(description = "프로필 이미지", example = "https://pallete-file.s3.ap-northeast-2.amazonaws.com/7c540940-8dcb-43e7-8a58-91c6066c860d.png")
   private String profileImage;
 
   @Builder
-  public UserUpdateDto(String email, String name, String phone, boolean isAlarmAccept, String profileImage) {
-    this.email = email;
-    this.name = name;
-    this.phone = phone;
-    this.isAlarmAccept = isAlarmAccept;
+  public UserUpdateDto(String nickname, String profileImage) {
+    this.nickname = nickname;
     this.profileImage = profileImage;
   }
 }
