118ce51302f04ea2afd5042dbda4ef8ea6421fb1
djhek <djheksa@gmail.com>
2024-02-09 10:12:15 +0900

    Revert "feat : main-home today artists"
    This reverts commit 4b2598080604261bceea9688c781bb60811072c1.

--- src/main/java/com/study/palette/module/mainHome/controller/MainHomeController.java
+++ src/main/java/com/study/palette/module/mainHome/controller/MainHomeController.java
@@ -1,9 +1,6 @@
 package com.study.palette.module.mainHome.controller;
 
-import com.study.palette.common.dto.PaginationDto;
-import com.study.palette.module.mainHome.dto.TodayMusiciansResponseDto;
 import com.study.palette.module.mainHome.dto.TopMusiciansResponseDto;
-import com.study.palette.module.mainHome.dto.query.FindTodayMusicianQuery;
 import com.study.palette.module.mainHome.dto.query.FindTopMusicianQuery;
 import com.study.palette.module.mainHome.service.MainHomeService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +10,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,12 +34,12 @@ public class MainHomeController {
   }
 
   //오늘의 아티스트
-  @Operation(summary = "모든 서비스 오늘의 아티스트 조회", description = "모든 서비스에서 오늘의 아티스트를 조회합니다.")
-  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TodayMusiciansResponseDto.class)))), @ApiResponse(responseCode = "400", description = "Bad Request")})
-  @GetMapping("/today")
-  public PaginationDto<TodayMusiciansResponseDto> todayArtistInfos(FindTodayMusicianQuery query) {
-    return mainHomeService.getTodayArtists(query, query.toPageable(Sort.by(Sort.Direction.DESC, "createdAt")));
-  }
+//  @Operation(summary = "모든 서비스 오늘의 아티스트 조회", description = "모든 서비스에서 오늘의 아티스트를 조회합니다.")
+//  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TodayMusiciansResponseDto.class)))), @ApiResponse(responseCode = "400", description = "Bad Request")})
+//  @GetMapping("/today")
+//  public PaginationDto<TodayMusiciansResponseDto> todayArtistInfos(FindTodayMusicianQuery query) {
+//    return mainHomeService.getTodayArtists(query, query.toPageable(Sort.by(Sort.Direction.DESC, "createdAt")));
+//  }
 
 }
 
