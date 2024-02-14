da70db220fb16bb7097a5106be796dba252ee77a
djhek <djheksa@gmail.com>
2024-02-14 14:14:31 +0900

    feat : token logging

--- src/main/java/com/study/palette/TokenGenerator.java
+++ src/main/java/com/study/palette/TokenGenerator.java
@@ -4,6 +4,8 @@ import com.study.palette.config.security.JwtTokenProvider;
 import com.study.palette.module.users.entity.Role;
 import com.study.palette.module.users.entity.Users;
 import com.study.palette.module.users.repository.UsersRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -34,15 +36,17 @@ public class TokenGenerator implements CommandLineRunner {
     Users user = usersRepository.findByEmail("testEmail").orElse(null);
     if (user != null) {
       log.info("이미 더미데이터가 존재합니다.");
-      Authentication authentication = jwtTokenProvider.getAuthentication(user.getId().toString());
-      log.info("MUSICIAN : \n" + jwtTokenProvider.createToken(authentication).getAccessToken());
-
+      String musicianToken = jwtTokenProvider.createToken(jwtTokenProvider.getAuthentication(user.getId().toString())).getAccessToken();
       Users adminUser = usersRepository.findByEmail("adminEmail").orElse(null);
-      Authentication adminAuthentication = jwtTokenProvider.getAuthentication(adminUser.getId().toString());
-      log.info("ADMIN : \n" + jwtTokenProvider.createToken(adminAuthentication).getAccessToken());
+      String adminToken = jwtTokenProvider.createToken(jwtTokenProvider.getAuthentication(adminUser.getId().toString())).getAccessToken();
+
+      log.info("MUSICIAN : \n" + musicianToken);
+      log.info("ADMIN : \n" + adminToken);
       return;
     }
     // 여기에 초기 데이터 생성 로직을 작성
+    List<Users> users = new ArrayList<>();
+
     Users musicianUsers = Users.builder()
         .email("testEmail")
         .password(passwordEncoder.encode("testPassword"))
@@ -53,7 +57,7 @@ public class TokenGenerator implements CommandLineRunner {
         .loginFailCount(0)
         .isLocked(false)
         .build();
-    usersRepository.save(musicianUsers);
+    users.add(musicianUsers);
 
     Users adminUsers = Users.builder()
         .email("adminEmail")
@@ -65,13 +69,17 @@ public class TokenGenerator implements CommandLineRunner {
         .loginFailCount(0)
         .isLocked(false)
         .build();
-    usersRepository.save(adminUsers);
+    users.add(adminUsers);
+
+    usersRepository.saveAll(users);
 
     Authentication musicianAuthentication = jwtTokenProvider.getAuthentication(musicianUsers.getId().toString());
     Authentication adminAuthentication = jwtTokenProvider.getAuthentication(adminUsers.getId().toString());
 
-    log.info("MUSICIAN : \n" + jwtTokenProvider.createToken(musicianAuthentication).getAccessToken());
-    log.info("ADMIN : \n" + jwtTokenProvider.createToken(adminAuthentication).getAccessToken());
+//    String musicianToken = jwtTokenProvider.createToken(musicianAuthentication).getAccessToken();
+//    String adminToken = jwtTokenProvider.createToken(adminAuthentication).getAccessToken();
 
+//    log.info("MUSICIAN : \n" + musicianToken);
+//    log.info("ADMIN : \n" + adminToken);
   }
 }
